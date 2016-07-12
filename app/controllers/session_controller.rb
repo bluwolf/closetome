104c4c4c6a176a381692201511d780a34753630a
Xiaohan Chen <xiaohanc@usc.edu>
2016-07-12 14:59:31 -0700

    fixed user authentication/bcrypt hashing error

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ class SessionController < ApplicationController
 
     def create
     @user = User.find_by_email(params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password_digest])
       session[:user_id] = @user.id
       redirect_to '/'
     else
